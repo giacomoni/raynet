1191022497b8bf63e444a2073c4adfdab052b37c
Luca G <giacomoni.l@gmail.com>
2022-11-28 19:34:58 +0000

    Fixed syntax error

--- installhelpers/installomnet.sh
+++ installhelpers/installomnet.sh
@@ -10,7 +10,7 @@ then
 	sudo apt-get install build-essential clang lld gdb bison flex perl \
 	python3 python3-pip qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools \
 	libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz \
-	libwebkit2gtk-4.0-37 && 
+	libwebkit2gtk-4.0-37 
 
 else
     echo "omnetpp-5.6.2 found in HOME directory. Building..."
