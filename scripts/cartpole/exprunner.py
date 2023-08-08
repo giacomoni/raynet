3dcbbb09bb67e589f67c458c8613b2509e519308
Luca G <l.giacomoni@sussex.ac.uk>
2023-08-08 22:37:25 +0100

    Experiment ready for omnetbind

--- scripts/cartpole/exprunner.py
+++ scripts/cartpole/exprunner.py
@@ -4,9 +4,17 @@ import os
 if __name__ == "__main__":
     
     SEEDS =  [1,10,100]
-    ENVS = ["OmnetGymApiEnv", "CartPole-v1"]
+    ENVS = ["ns3-v0","OmnetGymApiEnv", "CartPole-v1"]
     WORKERS = [2,4,8,16,32,63]
     
     
     for params in itertools.product(ENVS, WORKERS, SEEDS):
-         os.system(f"python3 cartpoleexp.py {params[0]} {params[1]} {params[2]}")
+        if params[0] != 'ns3-v0':
+            os.chdir(f"{os.getenv('HOME')}/raynet/scripts/cartpole")
+            os.system(f"python3 cartpoleexp.py {params[0]} {params[1]} {params[2]}")
+        else:
+            os.chdir(f"{os.getenv('HOME')}/ns-allinone-3.38/ns-3.38/contrib/opengym/examples/cartpole")
+            os.system(f"python3 cartpoleexp.py {params[0]} {params[1]} {params[2]}")
+
+
+    
